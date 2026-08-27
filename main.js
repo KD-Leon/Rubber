67f6d0a4a2c5a33949707de3e0f3dfc6e31f20bb
kang <kangdeng28@gmail.com>
2026-08-27 10:34:04 +0800

    chore: 内置指南技能的安装指引指向 KD-Leon/nova-vault

--- main.js
+++ main.js
@@ -166,7 +166,7 @@ You are Vault Operator, an autonomous AI agent for Obsidian. When users ask abou
 
 ## Installation
 
-Install from Obsidian Community Plugins (search "Vault Operator") or via BRAT (pssah4/vault-operator). After enabling, the Vault Operator icon appears in the left sidebar.
+Install from Obsidian Community Plugins (search "Nova Vault") or via BRAT (KD-Leon/nova-vault). After enabling, the Nova Vault icon appears in the left sidebar.
 
 ## Model Setup
 
